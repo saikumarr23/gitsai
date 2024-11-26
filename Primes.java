ca7164d5a3afba30ce56cc0be7c33bb9bb6fa120
saikumar <bajarusaikumar1@gmail.com>
2024-11-26 10:56:04 +0530

    Updated one

---
+++ Primes.java
@@ -0,0 +1,28 @@
+// public class Primes{    
+//  public static void main(String args[]){    
+//   int i,m=0,flag=0;      
+//   int n=3;//it is the number to be checked    
+//   m=n/2;      
+//   if(n==0||n==1){  
+//    System.out.println(n+" is not prime number");      
+//   }else{  
+//    for(i=2;i<=m;i++){      
+//     if(n%i==0){      
+//      System.out.println(n+" is not prime number");      
+//      flag=1;      
+//      break;      
+//     }      
+//    }      
+//    if(flag==0)  { System.out.println(n+" is prime number"); }  
+//   }//end of else  
+// } 
+// }   
+
+
+public class Primes{    
+ public static void main(String args[]){    
+  for(int i=0;i<10;i++){
+      System.out.println(i);
+  }
+}
+}
